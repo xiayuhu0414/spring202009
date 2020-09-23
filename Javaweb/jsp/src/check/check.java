f67b7c6bafdd5167a52d44c936285e2a14a30259
xiayuhu <jshzxiayuhu@126.com>
2020-09-23 18:21:00 +0800

    20200923

--- Javaweb/jsp/src/check/check.java
+++
@@ -1,9 +0,0 @@
-package check;
-
-public class check {
-    public boolean loginCheck(String xm, String mm) {
-        if (xm.equals("xyh") && mm.equals("123456")) {
-            return true;
-        } else return  false;
-    }
-}
